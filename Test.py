69513433f993b78b4f549a72293bf7caf19f0197
teja-dasari-prog <teja1687@gmail.com>
2025-01-17 14:08:51 -0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,41 +1,34 @@
 import dash
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output
 import plotly.express as px
-import pandas as pd
 
 app = dash.Dash(__name__)
-server=app.server
 
-# Create a sample DataFrame
-df = pd.DataFrame({
-    'Fruit': ['Apples', 'Oranges', 'Bananas', 'Apples', 'Oranges', 'Bananas'],
-    'Amount': [4, 1, 2, 2, 4, 5],
-    'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal']
-})
-
-# Create a bar chart
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-
+# Sample data
 df = px.data.gapminder()
-fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
-           size="pop", color="continent", hover_name="country", facet_col="continent",
-           log_x=True, size_max=45, range_x=[100,100000], range_y=[25,90])
 
-
-
-# Define the layout of the app
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+# App layout
+app.layout = html.Div([
+    html.H1("Dropdown Example"),
+    dcc.Dropdown(
+        id='country-dropdown',
+        options=[{'label': country, 'value': country} for country in df['country'].unique()],
+        value='Canada'  # Default value
+    ),
+    dcc.Graph(id='life-expectancy-graph')
 ])
+
+# Callback to update graph based on dropdown selection
+@app.callback(
+    Output('life-expectancy-graph', 'figure'),
+    Input('country-dropdown', 'value')
+)
+def update_graph(selected_country):
+    filtered_df = df[df['country'] == selected_country]
+    fig = px.line(filtered_df, x='year', y='lifeExp', title=f'Life Expectancy in {selected_country}')
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
