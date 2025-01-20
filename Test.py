685393d6cb4a3a24445c1d735f4ac0a4b0d8b790
teja-dasari-prog <teja1687@gmail.com>
2025-01-20 15:12:38 -0600

    Update Test_my _data.py

--- Test.py
+++ Test.py
@@ -5,31 +5,15 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 
 app = dash.Dash(__name__)
-
-# Sample data
-df = px.data.gapminder()
 server=app.server
 
-# App layout
-app.layout = html.Div([
-    html.H1("Dropdown Example"),
-    dcc.Dropdown(
-        id='country-dropdown',
-        options=[{'label': country, 'value': country} for country in df['country'].unique()],
-        value='Canada'  # Default value
-    ),
-    dcc.Graph(id='life-expectancy-graph')
-])
+# Load my data
+with shelve.open('data') as shelf:
+    loaded_data = shelf['my_data']
 
-# Callback to update graph based on dropdown selection
-@app.callback(
-    Output('life-expectancy-graph', 'figure'),
-    Input('country-dropdown', 'value')
-)
-def update_graph(selected_country):
-    filtered_df = df[df['country'] == selected_country]
-    fig = px.line(filtered_df, x='year', y='lifeExp', title=f'Life Expectancy in {selected_country}')
-    return fig
+fig = px.scatter(x='MET-farm', y='farm', data=loaded_data, hue='Month')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
+
+                       
