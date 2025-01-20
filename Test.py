4b98177296fefe7c7a7fd2b975430c77585783f4
teja-dasari-prog <teja1687@gmail.com>
2025-01-20 15:33:29 -0600

    Update Test_my_data.py

--- Test.py
+++ Test.py
@@ -11,7 +11,7 @@ server=app.server
 # Load my data
 loaded_data = pd.read_csv('loaded_data.csv')
 
-fig = px.scatter(x='MET-farm', y='farm', data=loaded_data, hue='Month')
+fig = px.scatter(loaded_data, x='MET-farm', y='farm', color='Month',title='Scatter Plot Example')
 
 if __name__ == "__main__":
     app.run_server(debug=True)
