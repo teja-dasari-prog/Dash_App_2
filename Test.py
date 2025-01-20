ca947425298ba72b71d4ad07ca7a47232a2e3e75
teja-dasari-prog <teja1687@gmail.com>
2025-01-20 15:43:43 -0600

    Update Test_my_data.py

--- Test.py
+++ Test.py
@@ -11,7 +11,14 @@ server=app.server
 # Load my data
 loaded_data = pd.read_csv('loaded_data.csv')
 
-fig = px.scatter(loaded_data, x='MET-farm', y='farm', color='Month',title='Scatter Plot Example')
+app.layout = html.Div([
+    html.H4("Interactive scatter plot with my data"),
+    dcc.Graph(id="scatter-plot"),
+])
+
+def update_scatter_plot(clickData):
+fig = px.scatter(loaded_data, x='MET-farm', y='farm',title='Scatter Plot Example')
+    return fig
 
 if __name__ == "__main__":
     app.run_server(debug=True)
