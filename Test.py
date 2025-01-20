1c76b9219f9e814a330c03767b32a67b88a56bf4
teja-dasari-prog <teja1687@gmail.com>
2025-01-20 17:47:59 -0600

    Update Test_my_data_v2.py

--- Test.py
+++ Test.py
@@ -29,7 +29,7 @@ app.layout = html.Div([
 
 def update_scatter_plot(clickData):
     filtered_loaded_data = loaded_data[loaded_data['Month'] == clickData]
-    fig = px.scatter(filtered_loaded_data, x='MET-farm', y='farm', title='Scatter Plot Example', trendline='ols',color='Week')
+    fig = px.scatter(filtered_loaded_data, x='MET-farm', y='farm', title='Scatter Plot Example', trendline='ols')
     fig.update_layout(
     xaxis_title="Avg hourly Irradiance (W/m$^2$)",
     yaxis_title="SS1 MWh (Sum hourly)"
